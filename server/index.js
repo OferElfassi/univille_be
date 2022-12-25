13be2a2c1c38dfabe2e9e03699f6b84a1454c7c1
ofer221 <ofer2212@gmail.com>
2022-12-25 21:29:03 +0200

    feat(server): add methods to player and fix types

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+  value: true,
 });
-Object.defineProperty(exports, "App", {
+Object.defineProperty(exports, 'App', {
   enumerable: true,
   get: function get() {
     return _App['default'];
-  }
+  },
 });
 
 var _App = _interopRequireDefault(require('./App'));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
